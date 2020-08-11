56b43dc04918ccbb9a285a10ca7a36c2ffd753ce
postables <postables@rtradetechnologies.com>
2020-08-11 16:27:40 -0700

    docs: update

--- html/d1/d22/logger_8c.js
+++ html/d1/d22/logger_8c.js
@@ -4,7 +4,7 @@ var logger_8c =
     [ "clear_thread_logger", "d1/d22/logger_8c.html#a8cf74430151bf53ab52b8b94788186af", null ],
     [ "debug_log", "d1/d22/logger_8c.html#adbd079d224c0a17760bd34be2b94d4d4", null ],
     [ "error_log", "d1/d22/logger_8c.html#a86974eb20c3c2a9b8528cca2bfd1526b", null ],
-    [ "get_time_string", "d1/d22/logger_8c.html#aa0609b044ca46a9e3d6e1ac8a6149422", null ],
+    [ "get_time_string", "d1/d22/logger_8c.html#af72d1c9f222fd1f9c9c03d2853805ccd", null ],
     [ "info_log", "d1/d22/logger_8c.html#acb4bc62c4f5124145fdd7813a8eff213", null ],
     [ "log_func", "d1/d22/logger_8c.html#a070efe5c00ac2e245b407b87a8314091", null ],
     [ "logf_func", "d1/d22/logger_8c.html#af467d23bd5262499508f556c4f530160", null ],
