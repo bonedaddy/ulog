a439fd6309403d938aef739af7a9ee85331ba157
bonedaddy <boned4ddy@protonmail.com>
2020-12-16 15:27:40 -0800

    update docs

--- html/search/defines_3.js
+++ html/search/defines_3.js
@@ -1,11 +1,11 @@
 var searchData=
 [
-  ['log_5fdebug_190',['LOG_DEBUG',['../d1/d8c/logger_8h.html#ad1946d2d37922323bc8800c6cfff91af',1,'logger.h']]],
-  ['log_5ferror_191',['LOG_ERROR',['../d1/d8c/logger_8h.html#a9d912cd3694fbcbdfb204b29f2345422',1,'logger.h']]],
-  ['log_5finfo_192',['LOG_INFO',['../d1/d8c/logger_8h.html#a663c757fa1a7b61074f6da01acabbd12',1,'logger.h']]],
-  ['log_5fwarn_193',['LOG_WARN',['../d1/d8c/logger_8h.html#a10ef0ebfb4079b7270f555fae9a10e12',1,'logger.h']]],
-  ['logf_5fdebug_194',['LOGF_DEBUG',['../d1/d8c/logger_8h.html#a55930c941161b084e39cd6436de028f0',1,'logger.h']]],
-  ['logf_5ferror_195',['LOGF_ERROR',['../d1/d8c/logger_8h.html#a7bdb2ee39fb10f7ced0e14735158aba4',1,'logger.h']]],
-  ['logf_5finfo_196',['LOGF_INFO',['../d1/d8c/logger_8h.html#a29dbd2356dc6f23cdba5fafde6873771',1,'logger.h']]],
-  ['logf_5fwarn_197',['LOGF_WARN',['../d1/d8c/logger_8h.html#a5d765dc8f0829d66174c23413539db0c',1,'logger.h']]]
+  ['log_5fdebug_190',['LOG_DEBUG',['../d1/d8c/logger_8h.html#a4081d3e232178e98369fd7d79232ad69',1,'logger.h']]],
+  ['log_5ferror_191',['LOG_ERROR',['../d1/d8c/logger_8h.html#a4aff853658d35da1dcca2b1f73ee608c',1,'logger.h']]],
+  ['log_5finfo_192',['LOG_INFO',['../d1/d8c/logger_8h.html#ab6dda8962b3eb3690df612cf2e772c9d',1,'logger.h']]],
+  ['log_5fwarn_193',['LOG_WARN',['../d1/d8c/logger_8h.html#ad505b4b8d1be5fa7af03c5495aa2390c',1,'logger.h']]],
+  ['logf_5fdebug_194',['LOGF_DEBUG',['../d1/d8c/logger_8h.html#a0e84e32acc9643094477f4b7323c10de',1,'logger.h']]],
+  ['logf_5ferror_195',['LOGF_ERROR',['../d1/d8c/logger_8h.html#a6ca251b9df8bc1bc3f497acc5f56a61a',1,'logger.h']]],
+  ['logf_5finfo_196',['LOGF_INFO',['../d1/d8c/logger_8h.html#af7d6085ce56c16ed9bb841a8b82e1caf',1,'logger.h']]],
+  ['logf_5fwarn_197',['LOGF_WARN',['../d1/d8c/logger_8h.html#a3f4bbc5e6b0861a9b9ee2aa1f76014b2',1,'logger.h']]]
 ];
